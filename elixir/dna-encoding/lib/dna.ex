74699460d7ebcdbc20e8f13bd0510631fbe11c47
Dane Burns <daneb91@gmail.com>
2021-11-06 10:31:52 -0700

    non-tail recursive impl

--- elixir/dna-encoding/lib/dna.ex
+++ elixir/dna-encoding/lib/dna.ex
@@ -1,14 +1,31 @@
 defmodule DNA do
   def encode_nucleotide(code_point) do
-    # Please implement the encode_nucleotide/1 function
+    case code_point do
+      65 -> 0b0001
+      67 -> 0b0010
+      71 -> 0b0100
+      84 -> 0b1000
+      32 -> 0b0000
+      _ -> :error
+    end
   end
 
   def decode_nucleotide(encoded_code) do
-    # Please implement the decode_nucleotide/1 function
+    case encoded_code do
+      0b0001 -> 65
+      0b0010 -> 67
+      0b0100 -> 71
+      0b1000 -> 84
+      0b0000 -> 32
+      _ -> :error
+    end
   end
 
-  def encode(dna) do
-    # Please implement the encode/1 function
+  def encode([head | tail]) do
+    encode([head]) + encode(tail)
+  end
+
+  def encode([head]) do
   end
 
   def decode(dna) do
